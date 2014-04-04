47f80ba41d72e96bca58f581cda98a89d4e5c9e3
Matt Gauger <matt.gauger@gmail.com>
2014-04-03 20:27:22 -0500

    Refactor gmail ingestion functionality out into new namespace

--- src/autodjinn/core.clj
+++ src/autodjinn/core.clj
@@ -1,7 +1,5 @@
 (ns autodjinn.core
-  (:require [clojure-mail.core :refer :all]
-            [clojure-mail.message :as message :refer [read-message]]
-            [nomad :refer [defconfig]]
+  (:require [nomad :refer [defconfig]]
             [clojure.java.io :as io]
             [datomic.api :as d]))
 
@@ -87,78 +85,3 @@
 
 (defn update-schema []
   (d/transact db-connection schema-txn))
-
-(defn get-sent-date
-  "Returns an instant for the date sent"
-  [msg]
-  (.getSentDate msg))
-
-(defn get-received-date
-  "Returns an instant for the date sent"
-  [msg]
-  (.getReceivedDate msg))
-
-(defn cc-list
-  "Returns a sequence of CC-ed recipients"
-  [msg]
-  (map str
-    (.getRecipients msg javax.mail.Message$RecipientType/CC)))
-
-(defn bcc-list
-  "Returns a sequence of BCC-ed recipients"
-  [msg]
-  (map str
-    (.getRecipients msg javax.mail.Message$RecipientType/BCC)))
-
-(defn simple-content-type [full-content-type]
-  (-> full-content-type
-      (clojure.string/split #"[;]")
-      (first)
-      (clojure.string/lower-case)))
-
-(defn is-content-type? [body requested-type]
-  (= (simple-content-type (:content-type body))
-     requested-type))
-
-(defn find-body-of-type [bodies type]
-  (:body (first (filter #(is-content-type? %1 type) bodies))))
-
-(defn get-text-body [msg]
-  (find-body-of-type (message/message-body msg) "text/plain"))
-
-(defn get-html-body [msg]
-  (find-body-of-type (message/message-body msg) "text/html"))
-
-(defn remove-angle-brackets
-  [string]
-  (-> string
-      (clojure.string/replace ">" "")
-      (clojure.string/replace "<" "")))
-
-(def my-store (gen-store gmail-username gmail-password))
-
-(defn ingest-inbox []
-  (doseq [msg (inbox my-store)]
-    (println (message/subject msg))
-    @(d/transact db-connection [{:db/id (d/tempid "db.part/user")
-                                 :mail/uid (remove-angle-brackets (message/id msg))
-                                 :mail/from (message/from msg)
-                                 :mail/to (message/to msg)
-                                 :mail/cc (cc-list msg)
-                                 :mail/bcc (bcc-list msg)
-                                 :mail/subject (message/subject msg)
-                                 :mail/date-sent (get-sent-date msg)
-                                 :mail/date-received (get-received-date msg)
-                                 :mail/text-body (get-text-body msg)
-                                 :mail/html-body (get-html-body msg)}])))
-
-(defn -main
-  "Perform a Gmail ingestion"
-  []
-  (println "Gmail ingestion starting up")
-  (println "Attmpting to update the schema")
-  (update-schema)
-  (println "Beginning email ingestion")
-  (ingest-inbox)
-  (println "Done ingesting")
-  (System/exit 0))
---
+++ src/autodjinn/gmail_ingestion.clj
@@ -0,0 +1,87 @@
+(ns autodjinn.gmail-ingestion
+  (:require [autodjinn.core :refer :all]
+            [clojure-mail.core :refer :all]
+            [clojure-mail.message :as message :refer [read-message]]
+            [nomad :refer [defconfig]]
+            [clojure.java.io :as io]
+            [datomic.api :as d]))
+
+(defconfig mail-config (io/resource "config/autodjinn-config.edn"))
+
+(def gmail-username (get (mail-config) :gmail-username))
+(def gmail-password (get (mail-config) :gmail-password))
+
+(defn get-sent-date
+  "Returns an instant for the date sent"
+  [msg]
+  (.getSentDate msg))
+
+(defn get-received-date
+  "Returns an instant for the date sent"
+  [msg]
+  (.getReceivedDate msg))
+
+(defn cc-list
+  "Returns a sequence of CC-ed recipients"
+  [msg]
+  (map str
+    (.getRecipients msg javax.mail.Message$RecipientType/CC)))
+
+(defn bcc-list
+  "Returns a sequence of BCC-ed recipients"
+  [msg]
+  (map str
+    (.getRecipients msg javax.mail.Message$RecipientType/BCC)))
+
+(defn simple-content-type [full-content-type]
+  (-> full-content-type
+      (clojure.string/split #"[;]")
+      (first)
+      (clojure.string/lower-case)))
+
+(defn is-content-type? [body requested-type]
+  (= (simple-content-type (:content-type body))
+     requested-type))
+
+(defn find-body-of-type [bodies type]
+  (:body (first (filter #(is-content-type? %1 type) bodies))))
+
+(defn get-text-body [msg]
+  (find-body-of-type (message/message-body msg) "text/plain"))
+
+(defn get-html-body [msg]
+  (find-body-of-type (message/message-body msg) "text/html"))
+
+(defn remove-angle-brackets
+  [string]
+  (-> string
+      (clojure.string/replace ">" "")
+      (clojure.string/replace "<" "")))
+
+(def my-store (gen-store gmail-username gmail-password))
+
+(defn ingest-inbox []
+  (doseq [msg (inbox my-store)]
+    (println (message/subject msg))
+    @(d/transact db-connection [{:db/id (d/tempid "db.part/user")
+                                 :mail/uid (remove-angle-brackets (message/id msg))
+                                 :mail/from (message/from msg)
+                                 :mail/to (message/to msg)
+                                 :mail/cc (cc-list msg)
+                                 :mail/bcc (bcc-list msg)
+                                 :mail/subject (message/subject msg)
+                                 :mail/date-sent (get-sent-date msg)
+                                 :mail/date-received (get-received-date msg)
+                                 :mail/text-body (get-text-body msg)
+                                 :mail/html-body (get-html-body msg)}])))
+
+(defn -main
+  "Perform a Gmail ingestion"
+  []
+  (println "Gmail ingestion starting up")
+  (println "Attmpting to update the schema")
+  (update-schema)
+  (println "Beginning email ingestion")
+  (ingest-inbox)
+  (println "Done ingesting")
+  (System/exit 0))
