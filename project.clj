f792a1e7d51cc3592174e07e8de9dc24f57e5849
Matt Gauger <matt.gauger@gmail.com>
2014-10-22 13:42:07 -0500

    Upgrade nomad

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.datomic/datomic-pro "0.9.4384"]
                  [clojure-mail "0.1.6"]
-                 [jarohen/nomad "0.6.3"]]
+                 [jarohen/nomad "0.7.0"]]
   :plugins [[lein-datomic "0.2.0"]]
   :profiles {:dev
              {:datomic {:config "resources/sql-transactor-template.properties"
